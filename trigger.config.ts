866c35b1d4410050ee2edd056e0005d84e7737df
Noobie31 <yuvan.msky@gmail.com>
2026-02-24 05:48:59 +0530

    feat: Add ffmpeg and ffprobe static binaries to the additional files configuration.

--- trigger.config.ts
+++ trigger.config.ts
@@ -11,5 +11,12 @@ export default defineConfig({
       "fluent-ffmpeg",
       "sharp",
     ],
+    additionalFiles: [
+      "./node_modules/ffmpeg-static/ffmpeg",
+      "./node_modules/ffprobe-static/bin/linux/x64/ffprobe",
+      "./node_modules/ffprobe-static/bin/linux/ia32/ffprobe",
+      "./node_modules/ffprobe-static/bin/darwin/x64/ffprobe",
+      "./node_modules/ffprobe-static/bin/win32/x64/ffprobe.exe",
+    ],
   },
 })
