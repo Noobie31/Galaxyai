806792da3bed40b8792cbd9523c264a32624158c
Noobie31 <yuvan.msky@gmail.com>
2026-02-24 21:29:50 +0530

    triger fixed

--- trigger.config.ts
+++ trigger.config.ts
@@ -7,16 +7,7 @@ export default defineConfig({
   build: {
     external: [
       "ffmpeg-static",
-      "ffprobe-static",
       "fluent-ffmpeg",
-      "sharp",
-    ],
-    additionalFiles: [
-      "./node_modules/ffmpeg-static/ffmpeg",
-      "./node_modules/ffprobe-static/bin/linux/x64/ffprobe",
-      "./node_modules/ffprobe-static/bin/linux/ia32/ffprobe",
-      "./node_modules/ffprobe-static/bin/darwin/x64/ffprobe",
-      "./node_modules/ffprobe-static/bin/win32/x64/ffprobe.exe",
     ],
   },
 })
